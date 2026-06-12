46a0f1e77c1345c4ed1d2aae0f561933acc7a13d
cortezrk <cortezrk@users.noreply.github.com>
2026-06-12 22:26:39 +0700

    fix: exclude reset api from auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: ["/((?!login|register|api/auth|_next/static|_next/image|favicon.ico|logo\\.jpg).*)"],
+  matcher: ["/((?!login|register|api/auth|api/reset|_next/static|_next/image|favicon.ico|logo\\.jpg).*)"],
 }
